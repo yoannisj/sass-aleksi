9d75ca822f121714bbad96ed0454da25a1cc7ed9
Yoannis Jamar <yoannis.j@gmail.com>
2015-11-26 15:02:43 +0100

    updated version number
    Signed-off-by: Yoannis Jamar <yoannis.j@gmail.com>

--- lib/sass-aleksi.rb
+++ lib/sass-aleksi.rb
@@ -26,6 +26,6 @@ end
 # Version is a number. If a version contains alphas, it will be created as a prerelease version
 # Date is in the form of YYYY-MM-DD
 module SassAleksi
-    VERSION = "0.2.1"
-    DATE = "2015-08-26"
+    VERSION = "0.2.2"
+    DATE = "2015-10-26"
 end
