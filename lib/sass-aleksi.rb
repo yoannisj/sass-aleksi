c0c3b9c960366930ff70e2168d7424cf86f027ac
Yoannis Jamar <yoannis.j@gmail.com>
2016-10-02 18:59:09 +0200

    better gemspec

--- lib/sass-aleksi.rb
+++ lib/sass-aleksi.rb
@@ -1,7 +1,6 @@
 # Require additional compass plugins here
-require 'compass/import-once/activate'
-require 'SassyLists'
-require 'sassy-maps'
+require 'sass-aleski/version'
+require 'sass'
 
 # Extension Paths
 extension_path = File.expand_path(File.join(File.dirname(__FILE__), ".."))
@@ -20,9 +19,5 @@ else
     end
 end
 
-# Version is a number. If a version contains alphas, it will be created as a prerelease version
-# Date is in the form of YYYY-MM-DD
 module SassAleksi
-    VERSION = "0.2.4"
-    DATE = "2016-10-02"
 end
