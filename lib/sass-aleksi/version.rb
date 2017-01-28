0d604c8f0a5d46664ed9d2d980955d74ab561c17
Yoannis Jamar <yoannis.j@gmail.com>
2017-01-28 17:29:11 +0100

    bump version

--- lib/sass-aleksi/version.rb
+++ lib/sass-aleksi/version.rb
@@ -1,6 +1,6 @@
 # Version is a number. If a version contains alphas, it will be created as a prerelease version
 # Date is in the form of YYYY-MM-DD
 module SassAleksi
-    VERSION = "0.3.1"
-    DATE = "2016-10-08"
+    VERSION = "0.3.2"
+    DATE = "2017-01-28"
 end
