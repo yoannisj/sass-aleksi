2c45aaea798f908922baec601cd8149aa168935b
Yoannis Jamar <yoannis.j@gmail.com>
2017-01-28 18:20:05 +0100

    cleanup

--- lib/sass-aleksi/version.rb
+++ lib/sass-aleksi/version.rb
@@ -1,6 +1,6 @@
 # Version is a number. If a version contains alphas, it will be created as a prerelease version
 # Date is in the form of YYYY-MM-DD
 module SassAleksi
-    VERSION = "0.3.3"
+    VERSION = "0.3.4"
     DATE = "2017-01-28"
 end
